26310bea71d177923677a5827bd3bb73d286d549
ParthGadhiya1999 <parth.gadhiya@einfochips.com>
2022-03-16 13:29:48 +0530

    All Done

--- FoodShades.Web/Controllers/ProductsController.cs
+++ FoodShades.Web/Controllers/ProductsController.cs
@@ -1,22 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Azure.Storage.Blobs;
+using FoodShades.Web.ViewModels;
+using FoodShades.Web.Models;
+using FoodShades.Web.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using FoodShades.Web.Data;
-using FoodShades.Web.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 
 namespace FoodShades.Web.Controllers
 {
+   
     public class ProductsController : Controller
     {
-        private readonly ApplicationDbContext _context;
+        private const string BlobContainerNAME = "productimages";
 
-        public ProductsController(ApplicationDbContext context)
+        private readonly ApplicationDbContext _context;
+        private readonly IConfiguration _config;
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(
+            ApplicationDbContext context,
+            IConfiguration config,
+            ILogger<ProductsController> logger)
         {
             _context = context;
+            _config = config;
+            _logger = logger;
         }
 
         // GET: Products
@@ -57,16 +73,35 @@ namespace FoodShades.Web.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ProductId,ProductName,ProductDescription,Price,UnitOfMeasure,Size,ProductImageFileUrl,ProductImageContentType,CategoryId")] Product product)
+        public async Task<IActionResult> Create([Bind("ProductId,ProductName,ProductDescription,Price,UnitOfMeasure,Size,ProductImageFileUrl,ProductImageContentType,CategoryId,Photo")] ProductViewModel productViewModel)
         {
             if (ModelState.IsValid)
             {
-                _context.Add(product);
+                string photoUrl = null;
+                if (productViewModel.Photo != null)
+                {
+                    // Upload the product image to the Blob Storage Account.
+                    photoUrl = await SavePhotoToBlobAsync(productViewModel.Photo);
+                }
+
+                Product newProduct = new Product
+                {
+                    ProductId = productViewModel.ProductId,
+                    CategoryId = productViewModel.CategoryId,
+                    ProductName = productViewModel.ProductName,
+                    ProductDescription = productViewModel.ProductDescription,
+                    Price = productViewModel.Price,
+                    UnitOfMeasure = productViewModel.UnitOfMeasure,
+                    Size = productViewModel.Size,
+                    ProductImageFileUrl = photoUrl,
+                    ProductImageContentType = photoUrl == null ? null : productViewModel.Photo.ContentType
+                };
+                _context.Add(newProduct);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["CategoryId"] = new SelectList(_context.Category, "CategoryId", "CategoryName", product.CategoryId);
-            return View(product);
+            ViewData["CategoryId"] = new SelectList(_context.Category, "CategoryId", "CategoryName", productViewModel.CategoryId);
+            return View(productViewModel);
         }
 
         // GET: Products/Edit/5
@@ -82,8 +117,19 @@ namespace FoodShades.Web.Controllers
             {
                 return NotFound();
             }
-            ViewData["CategoryId"] = new SelectList(_context.Category, "CategoryId", "CategoryName", product.CategoryId);
-            return View(product);
+            var productViewModel = new ProductViewModel
+            {
+                ProductId = product.ProductId,
+                CategoryId = product.CategoryId,
+                ProductName = product.ProductName,
+                ProductDescription = product.ProductDescription,
+                Price = product.Price,
+                UnitOfMeasure = product.UnitOfMeasure,
+                Size = product.Size
+            };
+            ViewBag.ProuctImageFileUrl = product.ProductImageFileUrl;
+            ViewData["CategoryId"] = new SelectList(_context.Category, "CategoryId", "CategoryName", productViewModel.CategoryId);
+            return View(productViewModel);
         }
 
         // POST: Products/Edit/5
@@ -91,15 +137,33 @@ namespace FoodShades.Web.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ProductId,ProductName,ProductDescription,Price,UnitOfMeasure,Size,ProductImageFileUrl,ProductImageContentType,CategoryId")] Product product)
+        public async Task<IActionResult> Edit(int id, [Bind("ProductId,ProductName,ProductDescription,Price,UnitOfMeasure,Size,ProductImageFileUrl,ProductImageContentType,CategoryId,Photo")] ProductViewModel productViewModel)
         {
-            if (id != product.ProductId)
+            if (id != productViewModel.ProductId)
             {
                 return NotFound();
             }
 
             if (ModelState.IsValid)
             {
+                string photoUrl = null;
+                if(productViewModel.Photo != null)
+                {
+                    photoUrl = await SavePhotoToBlobAsync(productViewModel.Photo);
+                }
+
+                var product = _context.Products.SingleOrDefault(p => p.ProductId == productViewModel.ProductId);
+                product.CategoryId = productViewModel.CategoryId;
+                product.ProductName = productViewModel.ProductName;
+                product.ProductDescription = productViewModel.ProductDescription;
+                product.Price = productViewModel.Price;
+                product.UnitOfMeasure = productViewModel.UnitOfMeasure;
+                product.Size = productViewModel.Size;
+                if(photoUrl != null)
+                {
+                    product.ProductImageFileUrl = photoUrl;
+                    product.ProductImageContentType = productViewModel.Photo.ContentType;
+                }
                 try
                 {
                     _context.Update(product);
@@ -118,8 +182,8 @@ namespace FoodShades.Web.Controllers
                 }
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["CategoryId"] = new SelectList(_context.Category, "CategoryId", "CategoryName", product.CategoryId);
-            return View(product);
+            ViewData["CategoryId"] = new SelectList(_context.Category, "CategoryId", "CategoryName", productViewModel.CategoryId);
+            return View(productViewModel);
         }
 
         // GET: Products/Delete/5
@@ -155,6 +219,48 @@ namespace FoodShades.Web.Controllers
         private bool ProductExists(int id)
         {
             return _context.Products.Any(e => e.ProductId == id);
+        }
+
+        private async Task<string> SavePhotoToBlobAsync(IFormFile productImage)
+        {
+            string storageConnection1 = _config.GetValue<string>("MyAzureSettings:StorageAccountKey1");
+            string storageConnection2 = _config.GetValue<string>("MyAzureSettings:StorageAccountKey2");
+            string fileName = productImage.FileName;
+            string tempFilePath = string.Empty;
+            string photoUrl;
+
+            if (productImage != null && productImage.Length > 0)
+            {
+                // Save the uploaded file on to a TEMP file.
+                tempFilePath = Path.GetTempFileName();
+                using (var stream = System.IO.File.Create(tempFilePath))
+                {
+                    productImage.CopyToAsync(stream).Wait();
+                }
+            }
+
+            // Get a reference to a container 
+            BlobContainerClient blobContainerClient = new BlobContainerClient(storageConnection1, BlobContainerNAME);
+
+            // Create the container if it does not exist - granting PUBLIC access.
+            await blobContainerClient.CreateIfNotExistsAsync(Azure.Storage.Blobs.Models.PublicAccessType.BlobContainer);
+
+            // Create the client to the Blob Item
+            BlobClient blobClient = blobContainerClient.GetBlobClient(fileName);
+
+            // Open the file and upload its data
+            using (FileStream uploadFileStream = System.IO.File.OpenRead(tempFilePath))
+            {
+                await blobClient.UploadAsync(uploadFileStream, overwrite: true);
+                uploadFileStream.Close();
+            }
+
+            // Delete the TEMP file since it is no longer needed.
+            System.IO.File.Delete(tempFilePath);
+
+            // Return the URI of the item in the Blob Storage
+            photoUrl = blobClient.Uri.ToString();
+            return photoUrl;
         }
     }
 }
